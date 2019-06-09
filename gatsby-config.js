3360adc8d2a5fc777740ce9d268411707d1c2340
Gopi Krishna <gopiwayz@gmail.com>
2019-06-09 13:41:39 +0530

    added icons and enabled offilne plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,12 +20,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Gopi Krishna`,
+        short_name: `GK`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        background_color: `#2296f3`,
+        theme_color: `#2296f3`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
@@ -39,6 +39,6 @@ module.exports = {
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 };
